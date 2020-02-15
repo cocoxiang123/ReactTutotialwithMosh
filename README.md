5f617859f0e8299a5121cf7de1c21d6e9814a7d1
Your Name <you@example.com>
2020-02-15 17:40:08 +1100

    update course practise

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 React Tutorial - Learn React - React Crash Course [2019]
 
 JSX
-Rendering Lists
-Consitional Rendering
-Handling Events
-Updating the State
+/Rendering Lists
+/Consitional Rendering
+/Handling Events
+/Updating the State
+/Props
+/Raising and handling events
+/lifting the state
+/functional components
+/lifecycle hooks
--- src/components/counter.js
+++ src/components/counter.js
@@ -2,50 +2,36 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: []
-  };
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tag.</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-  handleIncrement = id => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btm-lg"
         >
           Increment
         </button>
-        {this.renderTags()}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   formatCount = () =>
-    this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+    this.props.counter.value === 0 ? (
+      <h4>Zero</h4>
+    ) : (
+      <h4>{this.props.counter.value}</h4>
+    );
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
